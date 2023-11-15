04e347659c64989d867e8ba74a9328a3c33223f1
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2023-11-15 17:42:53 +0100

    switch to ebitkov/build-symfony-bundle action

--- .github/configure.php
+++ .github/configure.php
@@ -16,22 +16,3 @@ $config = \Symfony\Component\Yaml\Yaml::parseFile($fileName);
 $config['when@test']['twig']['paths']['%kernel.project_dir%/' . $bundlePath . '/tests/Fixtures'] = 'bundle_test';
 
 file_put_contents($fileName, \Symfony\Component\Yaml\Yaml::dump($config));
-
-
-### CONFIGURE COMPOSER ###
-
-$composer = json_decode(file_get_contents('composer.json'), true);
-
-// add bundle repo
-$composer['repositories']['bundle'] = [
-    'type' => 'path',
-    'url' => './' . $bundlePath,
-];
-
-// add dev autoload
-$composer['autoload-dev']['psr-4']['ebitkov\\BootstrapTwigComponents\\Tests\\'] = './' . $bundlePath . '/tests/';
-
-// unset, since it throws errors
-if (empty($composer['require-dev'])) unset($composer['require-dev']);
-
-file_put_contents('composer.json', json_encode($composer));
